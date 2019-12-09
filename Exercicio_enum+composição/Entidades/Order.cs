f3609608d55e5565b23c672747843f8e7fc047f7
Lucas <lucas_ssilva@outlook.com>
2019-12-09 17:14:21 -0200

    criação da classe order

---
+++ Exercicio_enum+composição/Entidades/Order.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_enum_composição.Entidades
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public Order() { }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double total = 0;
+            foreach(OrderItem item in Items)
+            {
+                total += item.SubTotal();
+            }
+            return total;
+        }
+    }
+}
--- Exercicio_enum+composição/Program.cs
+++ Exercicio_enum+composição/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Exercicio_enum_composição.Entidades;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace Exercicio_enum_composição
 {
